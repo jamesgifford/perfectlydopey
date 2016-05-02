eea41877c383a55539e7ff7e3048cd77da4cf8d3
James Gifford <jg.webdeveloper@gmail.com>
2016-05-02 15:34:41 -0700

    Updating public assets

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,28 @@
-function drawCharts(){drawChartPerfectYear(),drawChartPerfectAge(),drawChartPerfectGender(),drawChartPerfectState(),drawChartPerfectCountry()}google.charts.load("current",{packages:["corechart"]}),google.charts.setOnLoadCallback(drawCharts),$(window).resize(function(){this.resizeTO&&clearTimeout(this.resizeTO),this.resizeTO=setTimeout(function(){$(this).trigger("resizeEnd")},500)}),$(window).on("resizeEnd",function(){location.reload()});
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawCharts);
+
+function drawCharts() {
+    drawChartPerfectYear();
+    drawChartPerfectAge();
+    drawChartPerfectGender();
+    drawChartPerfectState();
+    drawChartPerfectCountry();
+};
+
+// create trigger to resizeEnd event     
+$(window).resize(function() {
+    if (this.resizeTO) {
+        clearTimeout(this.resizeTO);
+    }
+    this.resizeTO = setTimeout(function() {
+        $(this).trigger('resizeEnd');
+    }, 500);
+});
+
+// redraw graph when window resize is completed  
+$(window).on('resizeEnd', function() {
+    //location.reload(); // TODO: trigger a reload only if the window size changes too much, else do a redraw
+    //drawCharts();
+});
+
+//# sourceMappingURL=app.js.map
--- public/js/charts.js
+++ public/js/charts.js
@@ -1,1 +1,175 @@
-function drawChartPerfectAge(){var e=google.visualization.arrayToDataTable(PerfectlyDopey.countByAge),t={chart:{title:"Perfect Dopeys by Age"},chartArea:{left:35,top:0,width:"87%",height:"82%"},backgroundColor:{fill:"none"},legend:{position:"none"},vAxis:{title:"",format:""},hAxis:{title:"",format:""},theme:"material",bar:{groupWidth:"45%"}},o=new google.visualization.BarChart(document.getElementById("perfectAge"));o.draw(e,t)}function drawChartPerfectCountry(){var e=google.visualization.arrayToDataTable(PerfectlyDopey.countByCountry),t={chart:{title:"Perfect Dopeys by Country"},chartArea:{left:35,top:10,width:"90%",height:"88%"},backgroundColor:{fill:"none"},legend:{position:"none"},theme:"material",colorAxis:{colors:["blue","black"]}},o=new google.visualization.GeoChart(document.getElementById("perfectCountry"));o.draw(e,t)}function drawChartPerfectGender(){var e=google.visualization.arrayToDataTable(PerfectlyDopey.countByGender),t={chart:{title:"Perfect Dopeys by Gender"},chartArea:{left:35,top:10,width:"90%",height:"88%"},backgroundColor:{fill:"none"},legend:{position:"none"},theme:"material"},o=new google.visualization.PieChart(document.getElementById("perfectGender"));o.draw(e,t)}function drawChartPerfectState(){var e=google.visualization.arrayToDataTable(PerfectlyDopey.countByState),t={chart:{title:"Perfect Dopeys by State"},chartArea:{left:35,top:10,width:"90%",height:"88%"},backgroundColor:{fill:"none"},legend:{position:"none"},theme:"material",region:"US",resolution:"provinces",colorAxis:{colors:["blue","black"]}},o=new google.visualization.GeoChart(document.getElementById("perfectState"));o.draw(e,t)}function drawChartPerfectYear(){var e=new google.visualization.DataTable;e.addColumn("string","Year"),e.addColumn("number","Total"),e.addRows(PerfectlyDopey.countByYear);var t={chart:{title:"Perfect Dopeys by Year"},chartArea:{height:"88%",left:35,top:0,width:"82%"},backgroundColor:{fill:"none"},legend:{position:"none"},pointSize:10,lineWidth:5,vAxis:{title:"",format:"",maxValue:7e3,gridlines:{count:10},viewWindow:{max:7e3}},hAxis:{title:"",format:"",gridlines:{count:3,color:"#000"}},theme:"material"},o=new google.visualization.AreaChart(document.getElementById("perfectYear"));o.draw(e,t)}
+// Show count of Perfect Dopeys by Age
+function drawChartPerfectAge() {
+    var data = google.visualization.arrayToDataTable(PerfectlyDopey.countByAge);
+
+    var options = {
+        chart: {
+            title: 'Perfect Dopeys by Age'
+        },
+        chartArea: {
+            left: 35,
+            top: 0,
+            width: '87%',
+            height: '82%'
+        },
+        backgroundColor: {
+            fill: 'none'
+        },
+        legend: {
+            position: 'none'
+        },
+        vAxis: {
+            title: '',
+            format: ''
+        },
+        hAxis: {
+            title: '',
+            format: ''
+        },
+        theme: 'material',
+        bar: {
+            groupWidth: "45%"
+        }
+    };
+
+    var chart = new google.visualization.BarChart(document.getElementById('perfectAge'));
+    chart.draw(data, options);
+};
+// Show count of Perfect Dopeys by Country
+function drawChartPerfectCountry() {
+    var data = google.visualization.arrayToDataTable(PerfectlyDopey.countByCountry);
+
+    var options = {
+        chart: {
+            title: 'Perfect Dopeys by Country'
+        },
+        chartArea: {
+            left: 35,
+            top: 10,
+            width: '90%',
+            height: '88%'
+        },
+        backgroundColor: {
+            fill: 'none'
+        },
+        legend: {
+            position: 'none'
+        },
+        theme: 'material',
+        colorAxis: {
+            colors: ['blue', 'black']
+        }
+    };
+
+    var chart = new google.visualization.GeoChart(document.getElementById('perfectCountry'));
+    chart.draw(data, options);
+};
+// Show count of Perfect Dopeys by Gender
+function drawChartPerfectGender() {
+    var data = google.visualization.arrayToDataTable(PerfectlyDopey.countByGender);
+
+    var options = {
+        chart: {
+            title: 'Perfect Dopeys by Gender'
+        },
+        chartArea: {
+            left: 35,
+            top: 10,
+            width: '90%',
+            height: '88%'
+        },
+        backgroundColor: {
+            fill: 'none'
+        },
+        legend: {
+            position: 'none'
+        },
+        theme: 'material'
+    };
+
+    var chart = new google.visualization.PieChart(document.getElementById('perfectGender'));
+    chart.draw(data, options);
+};
+// Show count of Perfect Dopeys by State
+function drawChartPerfectState() {
+    var data = google.visualization.arrayToDataTable(PerfectlyDopey.countByState);
+
+    var options = {
+        chart: {
+            title: 'Perfect Dopeys by State'
+        },
+        chartArea: {
+            left: 35,
+            top: 10,
+            width: '90%',
+            height: '88%'
+        },
+        backgroundColor: {
+            fill: 'none'
+        },
+        legend: {
+            position: 'none'
+        },
+        theme: 'material',
+        region: "US",
+        resolution: "provinces",
+        colorAxis: {
+            colors: ['blue', 'black']
+        }
+    };
+
+    var chart = new google.visualization.GeoChart(document.getElementById('perfectState'));
+    chart.draw(data, options);
+};
+// Show count of Perfect Dopeys by Year
+function drawChartPerfectYear() {
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Year');
+    data.addColumn('number', 'Total');
+    data.addRows(PerfectlyDopey.countByYear);
+
+    var options = {
+        chart: {
+            title: 'Perfect Dopeys by Year'
+        },
+        chartArea: {
+            height: '88%',
+            left: 35,
+            top: 0,
+            width: '82%'
+        },
+        backgroundColor: {
+            fill: 'none'
+        },
+        legend: {
+            position: 'none'
+        },
+        pointSize: 10,
+        lineWidth: 5,
+        vAxis: {
+            title: '',
+            format: '',
+            maxValue: 7000,
+            gridlines: {
+                count: 10
+            },
+            viewWindow: {
+                max: 7000
+            }
+        },
+        hAxis: {
+            title: '',
+            format: '',
+            gridlines: {
+                count: 3,
+                color: '#000'
+            }
+        },
+        theme: 'material'
+    };
+
+    var chart = new google.visualization.AreaChart(document.getElementById('perfectYear'));
+    chart.draw(data, options);
+};
+
+//# sourceMappingURL=charts.js.map
